c1a243fc0f1f637c8e650a6b99cc123263d11a4e
Sevla <dasilvaalvesb@gmail.com>
2020-03-10 23:19:23 -0400

    grammar init

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import parser.*;
+import lexer.*;
+import node.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] arguments) {
+		System.out.println("Type a program: ");
+		Parser p = new Parser(new Lexer(new PushbackReader(new InputStreamReader(System.in), 1024)));
+		Node tree;
+		try {
+			tree = p.parse();
+			System.out.println("Thanks");
+		}
+		catch (IOException e) {
+			System.err.println("INPUT/OUTPUT ERROR: " + e.getMessage());
+			System.exit(1);
+		}
+		catch (LexerException e) {
+			System.err.println("LEXICAL ERROR: " + e.getMessage());
+			System.exit(1);
+		}
+		catch (ParserException e) {
+			System.err.println("SYNTAX ERROR: " + e.getMessage());
+			System.exit(1);
+		}
+	}
+}
