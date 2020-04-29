9681c267f83c2ee4e2f5918f530c87d33d483adc
Brandon ALVES BARBOSA <brandon.alves.barbosa@gmail.com>
2020-04-29 17:22:29 -0400

    review

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf src/analysis src/lexer src/node src/parser
-java -jar /usr/share/java/sablecc.jar -d src grammar/mu.sablecc
-javac -cp src -d bin src/Main.java
-java -cp bin Main < input/input1.mu
